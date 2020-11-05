96aff42e6e9d5afaae289433bb980d635fe28a79
Aleksey Vasiliyev <alekseyvy@gmail.com>
2020-11-05 17:08:07 +0300

    implemented return of random story from DB

--- project_NightFall_backend/apiComponents/getStoryComponent.js
+++ project_NightFall_backend/apiComponents/getStoryComponent.js
@@ -1,11 +1,20 @@
 const { sequelize, story} = require('../DB/index')
+const { Sequelize } = require('sequelize');
+
+
+
 sequelize.authenticate();
 console.log('Connection established successfully.')
 
+
+
 const getStory = async () => {
-    const res = await story.findAll()
+    const res = await story.findAll({
+        order: [ [ Sequelize.fn('RANDOM') ] ]
+    })
     return res
 }
+
 
 
 module.exports = getStory;
--- project_NightFall_backend/index.js
+++ project_NightFall_backend/index.js
@@ -2,12 +2,10 @@ const express = require('express')
 const getStory = require('./apiComponents/getStoryComponent.js')
 const asyncMiddleware = require('./middlewares/asyncMiddleware')
 
+
+
 const router = express()
 const port = 3000
-
-
-
-
 
 
 
@@ -15,26 +13,38 @@ const port = 3000
 //     res.send('Initial directory online')
 // })
 
+
+
 // router.get('/home', (req, res) => {
 //     res.send('/home directory online')
 // })
+
+
 
 router.get('/random_story', asyncMiddleware(async (req, res, next) => {
     const story = await getStory()
     res.json(story)
 }))
 
+
+
 // router.get('/ambient', (req, res) => {
 //     res.send('/ambient directory online')
 // })
+
+
 
 // router.post('/register', (req, res) => {
 //     res.send("/register API endpoint")
 // })
 
+
+
 // router.get('/login', (req, res) => {
 //     res.send("/login API endpoint")
 // })
+
+
 
 router.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
