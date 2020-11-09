bb32d47cd6aabe3e165e247670a3377ba468973d
Aleksey Vasiliyev <alekseyvy@gmail.com>
2020-11-09 17:49:09 +0300

    deploying frontend to netlifly

--- project_nightfall_frontend/src/viewComponents/registerComponent.js
+++ project_nightfall_frontend/src/viewComponents/registerComponent.js
@@ -1,89 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+const axios = require('axios');
 
 
 
-class RegisterComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: '',
-            e_mail: '',
-            password: ''
 
+const FormContainer = styled.div`
+display: flex;
+justify-content: center;
+align-self: center;
+width: 50%;
+border: 1px solid black;
+&:hover {
+    box-shadow: 10px 10px 10px;
+}
+`;
+
+const Button = styled.button`
+background: ${props => props.primary ? "palevioletred" : "white"};
+color: ${props => props.primary ? "white" : "palevioletred"};
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+`;
+
+function RegisterComponent() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const regObject = {
+            username: username,
+            email: email,
+            password: password
         }
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        axios.post('http://localhost:3000/register', regObject)
+        .then((res) => {
+            console.log(res)
+        })
     }
-
-    handleChange(event) {
-        this.setState({value: event.target.value})
-    }
-
-    handleSubmit(event) {
-        event.preventDefault()
-        alert('submitted' + this.state.value)
-    }
-
-
-
-    render(){
-        const FormContainer = styled.div`
-        display: flex;
-        justify-content: center;
-        align-self: center;
-        width: 50%;
-        border: 1px solid black;
-
-        &:hover {
-            box-shadow: 10px 10px 10px;
-        }
-        `;
-
-        const Button = styled.button`
-        background: ${props => props.primary ? "palevioletred" : "white"};
-        color: ${props => props.primary ? "white" : "palevioletred"};
-    
-        font-size: 1em;
-        margin: 1em;
-        padding: 0.25em 1em;
-        border: 2px solid palevioletred;
-        border-radius: 3px;
-        `;
 
         return (
-            <FormContainer>
-            <form onSubmit={this.handleSubmit}>
+            <FormContainer onSubmit={handleSubmit}>
+            <form >
                 <div>
                 <label>
                     Username:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" name="username" value={username}  onChange={(e) => setUsername(e.target.value)} />
                 </label>
                 </div>
                 <div>
                 <label>
                     E-mail:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" name="e_mail" value={email}  onChange={(e) => setEmail(e.target.value)} />
                 </label>
                 </div>
                 <div>
                 <label>
                     Password:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="password" name="password" value={password}  onChange={(e) => setPassword(e.target.value)} />
                 </label>
                 </div>
-            <div>
-            <label>
-                    Confirm password:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-            </div>
                 <Button type="submit" value="Submit">Submit</Button>
             </form>
             </FormContainer>
         )
     }
-}
 
 export default RegisterComponent;
