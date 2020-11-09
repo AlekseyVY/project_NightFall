1022c8afa23d8dce7fe90f407bf4f7725d95cb0c
Aleksey Vasiliyev <alekseyvy@gmail.com>
2020-11-09 20:48:09 +0300

    fixing login

--- services/tokenIssue.js
+++ services/tokenIssue.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
+
 
 
 const tokenIssue = (username) => {
-    const token = jwt.sign({ username: username}, process.env.ACESSTOKENSECRET)
+    const token = jwt.sign({ username: username}, 'supersecretpasswordissuetokenizationbadassprogrammerwtf')
     return token
 }
 
