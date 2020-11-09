be81484aef0a8f1c196467dd7be865e2af17ce38
Aleksey Vasiliyev <alekseyvy@gmail.com>
2020-11-09 20:34:03 +0300

    fixing login

--- apiComponents/userLoginComponent.js
+++ apiComponents/userLoginComponent.js
@@ -4,20 +4,27 @@ const tokenIssue = require('../services/tokenIssue')
 
 
 const getLogin = async ({ username, password }) => {
-
+    try {
         const res = await db.user.findAll({
             where: {
                 username: username
             }
         })
 
-        const checkResult = await checkPwd(password, res.data[0].password)
+        return res[0].dataValues.password
+
+        const checkResult = await checkPwd(password, res[0].dataValues.password)
         if(checkResult === true){
+            //return JWT from here
             const token = tokenIssue(username)
+            console.log(token)
             return token
         } else {
             return "wrong username or password"
         }
+    } catch(error) {
+        return "wrong username or password"
+    }
 }
 
 
