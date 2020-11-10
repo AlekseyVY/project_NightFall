0045f616ccfb0c75de64b4d613ad3a493ab2f48f
Aleksey Vasiliyev <alekseyvy@gmail.com>
2020-11-10 21:11:40 +0300

    added get story and render it into view for auth user

--- project_nightfall_frontend/src/services/localStorageService.js
+++ project_nightfall_frontend/src/services/localStorageService.js
@@ -17,3 +17,24 @@ export function getAuth(){
 export function removeToken() {
     localStorage.removeItem('token')
 }
+
+export function setStory(res){
+    localStorage.setItem('title', res.data[0].title)
+    localStorage.setItem('author', res.data[0].author)
+    localStorage.setItem('body', res.data[0].body)
+}
+
+export function getStory(){
+    const storyObject = {
+        title: localStorage.getItem('title'),
+        author: localStorage.getItem('author'),
+        body: localStorage.getItem('body')
+    }
+    return storyObject
+}
+
+export function deleteStory(){
+    localStorage.removeItem('title')
+    localStorage.removeItem('author')
+    localStorage.removeItem('body')
+}
--- project_nightfall_frontend/src/viewComponents/readerComponent.js
+++ project_nightfall_frontend/src/viewComponents/readerComponent.js
@@ -1,26 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getToken, getAuth } from "../services/localStorageService"
+import { setStory, getStory } from '../services/localStorageService'
 const axios = require('axios');
 
 
-function ReaderComponent() {
-    if(getAuth()) {
 
+function ReaderComponent() {
+    const [state, setState] = useState({})
+    if(getAuth()) {
         const token = getToken()
-        console.log(token)
-        axios.get('https://project-night-fall.herokuapp.com/random_story', { headers: {"Authorization": `Bearer ${token}`} })
-        .then(res => {
-            console.log(res.data.username.length)
-        })
+         axios.get('https://project-night-fall.herokuapp.com/random_story', { headers: {"Authorization": `Bearer ${token}`} })
+         .then(res => {
+            setStory(res)
+         })
+
+         function getStoryHandler(event) {
+             event.preventDefault()
+             setState(getStory())
+         }
+
+         console.log(state)
         return (
-        <div><h1>{getToken()}</h1></div>
+            <div>
+                <div>
+                    <h1>
+                        {state.title}
+                    </h1>
+                </div>
+                <div>
+                    <h4>
+                        {state.author}
+                    </h4>
+                </div>
+                <div>
+                    <p>
+                        {state.body}
+                    </p>
+                </div>
+                <button type="submit" value="Submit" onClick={getStoryHandler}>Read Story</button>
+            </div>
         )
     } else {
         return (
             <div><h1>Please Authorize for services.</h1></div>
         )
     }
-
 }
 
 
